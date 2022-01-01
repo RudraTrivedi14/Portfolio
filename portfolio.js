02dff4ee149db94c2edc0e7ad9bce7b05dca39eb
Rudra Trivedi <rct1408@gmail.com>
2022-01-01 11:11:49 +0530

    final javascript file of website

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,7 @@
 // menu show y hidden
 
 
-  const navMenu=document.getElementById('nav_menu');
+  const navMenu=document.getElementById('nav-menu');
   const navToggle=document.getElementById('nav-toggle');
   const navClose=document.getElementById('nav-close');
 
@@ -29,7 +29,7 @@ const navLink=document.querySelectorAll('.nav_link');
 
 
 function linkAction(){
-    const navMenu=document.getElementById('nav_menu');
+    const navMenu=document.getElementById('nav-menu');
     navMenu.classList.remove('show-menu');
 }
 
@@ -102,7 +102,85 @@ const modalViews= document.querySelectorAll('.services_modal'),
           });
       });
 
-   
+//    SCROLL SECTIONS ACTIVE LINK
+
+const sections=document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY=window.pageYOffset;
+
+    sections.forEach(current=>{
+        const sectionHeight=current.offsetHeight;
+        const sectionTop=current.offsetTop-50;
+        sectionId=current.getAttribute('id');
+
+        if(scrollY>sectionTop && scrollY<=sectionTop+sectionHeight){
+            document.querySelector('.nav_menu a[href*='+sectionId+']').classList.add('active-link');
+        }
+        else{
+            document.querySelector('.nav_menu a[href*='+sectionId+']').classList.remove('active-link');
+        }
+    })
+}
+window.addEventListener('scroll',scrollActive);
+
+
+// CHANGE BACKGROUND HEADER
+
+function scrollHeader(){
+    const nav=document.getElementById('header');
+    if(this.scrollY>=80){
+        nav.classList.add('scroll-header');
+    }
+    else{
+        nav.classList.remove('scroll-header');
+    }
+}
+
+window.addEventListener('scroll',scrollHeader);
+
+
+// SHOW SCROLL UP
+
+function scrollUp(){
+    const scrollUp=document.getElementById('scroll-up');
+    if(this.scrollY>=560){
+        scrollUp.classList.add('show-scroll');
+    }
+
+    else{
+        scrollUp.classList.remove('show-scroll');
+    }
+}
+
+window.addEventListener('scroll',scrollUp);
+
+// DARK-LIGHT THEME
+
+const themeButton=document.getElementById('theme-button');
+const darkTheme='dark-theme';
+const iconTheme='uil-sun';
+
+const selectedTheme=localStorage.getItem('selected-theme');
+const selectedIcon=localStorage.getItem('selected-icon');
+
+const getCurrentTheme=()=>document.body.classList.contains(darkTheme) ? 'dark':'light'
+const getCurrentIcon=()=>themeButton.classList.contains(iconTheme) ? 'uil-moon':'uil-sun'
+
+if(selectedTheme){
+    document.body.classList[selectedTheme==='dark' ? 'add' :'remove'](darkTheme)
+    themeButton.classList[selectedIcon==='uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click',()=>{
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme',getCurrentTheme())
+    localStorage.setItem('selected-icon',getCurrentIcon())
+})
+
+
 
 
 
